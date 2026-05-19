d1f429365f331d513c7dfe997d858ab93fe86f54
Manvir kaur <manvirkaur8193@gmail.com>
2026-05-19 09:08:47 -0500

    added css files

--- src/components/Dialog.js
+++ src/components/Dialog.js
@@ -2,7 +2,7 @@ function Dialog(props) {
   return (
     <div className="dialog">
       <div className="dialog-box">
-          <img src="https://picryl.com/media/the-forest-at-sunset-unsplash-a0e8ed" alt="dialog"/>
+          <img src="https://images.unsplash.com/photo-1501785888041-af3ef285b470" alt="dialog"/>
         <button onClick={props.close}>Close</button>
       </div>
     </div>
--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -4,16 +4,16 @@ import Star from "./Star";
 function StarRating() {
   const [starRating, setStarRating] = useState(0);
 
-  const feedback = ["Bad", "Ok", "Good", "Very Good", "Excellent"];
+  const feedback = ["Select Rating", "Bad", "Ok", "Good", "Very Good", "Excellent"];
  
   return (
    <div className="star-container">
     {[1, 2, 3, 4, 5].map((star) => (
-    <Star
-      key={star}
-      selected={star <= starRating}
-      onSelect={() => setStarRating(star)} />
-    ))}
+      <Star
+        key={star}
+        selected={star <= starRating}
+        onSelect={() => setStarRating(star)} />
+      ))}
     <h2>{feedback[starRating]}</h2>
    </div>
   );
