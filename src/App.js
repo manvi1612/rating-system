68f12761ed8648bbb6d8b22b3cbb1b9f85f1a79a
Manvir kaur <manvirkaur8193@gmail.com>
2026-05-18 22:54:14 -0500

    added app.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,23 @@ import StarRating from "./components/StarRating";
 import Dialog from "./components/Dialog";
 
 function App() {
+  const [dialog, setDialog] = useState(false);
+     function open() {
+      setDialog(true);
+    }
+    function close() {
+      setDialog(false);
+    }
+
   return (
-   <></>
+     <div className="container">
+      <h1>Star Rating</h1>
+      <StarRating />
+      <button onClick={() => setDialog(true)}>Open Dialog</button>
+      {dialog && (
+        <dialog close={() => setDialog(false)} />
+        )}
+    </div>
   );
 }
 
