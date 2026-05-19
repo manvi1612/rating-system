2c440438666bbb44bad00e96b62b33c705f893ea
Manvir kaur <manvirkaur8193@gmail.com>
2026-05-18 22:21:00 -0500

    added starrating component

--- src/components/Star.js
+++ src/components/Star.js
@@ -1,9 +1,11 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
-function Star() {
-  return 
+function Star(props) {
+  return (
   <FontAwesomeIcon
     icon={faStar} 
-    className="star" />;
+    className={props.selected? "star selected" : "star"}
+    onClick={props.onClick}/>
+  );
 }
 export default Star;
--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -1,7 +1,18 @@
-
+import { useState } from "react";
+import Star from "./Star";
 function starRating() {
+  const [starRating, setStarRating] = useState(0);
+  const feedback = ["Bad", "Ok", "Good", "Very Good", "Excellent"];
   return (
-   <></>
+   <div className="star-container">
+    {[1, 2, 3, 4, 5].map((star) => (
+    <Star
+      key={star}
+      selected={star <= rating}
+      onSelect={() => setStarRating(star)} />
+    ))}
+    <h2>{feedback[rating]}</h2>
+   </div>
   );
 }
 
